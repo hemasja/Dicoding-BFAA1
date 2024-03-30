58debe1fdcef1f088d511ae691568ef490d34317
Muhammad Hemas Julang Arrasy <91818681+hemasja@users.noreply.github.com>
2024-03-30 12:34:39 +0700

    EDIT: Connecting API

--- app/src/main/java/com/example/dicoding_bfaa1/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/dicoding_bfaa1/data/retrofit/ApiService.kt
@@ -1,10 +1,11 @@
 package com.example.dicoding_bfaa1.data.retrofit
 
+import com.example.dicoding_bfaa1.BuildConfig
 import com.example.dicoding_bfaa1.data.response.GithubResponse
 import retrofit2.Call
 import retrofit2.http.*
 interface ApiService {
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     @GET("search/users")
         fun getGithubUsers(
             @Query("q") user: String
