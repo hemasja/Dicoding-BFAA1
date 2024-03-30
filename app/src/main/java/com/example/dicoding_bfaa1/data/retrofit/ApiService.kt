e3db3ae36583dd9661b1b23adb26eb624d98d89e
Muhammad Hemas Julang Arrasy <91818681+hemasja@users.noreply.github.com>
2024-03-30 13:18:26 +0700

    ADD: Adding Search Bar

--- app/src/main/java/com/example/dicoding_bfaa1/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/dicoding_bfaa1/data/retrofit/ApiService.kt
@@ -5,7 +5,7 @@ import com.example.dicoding_bfaa1.data.response.GithubResponse
 import retrofit2.Call
 import retrofit2.http.*
 interface ApiService {
-    @Headers("Authorization: token ${BuildConfig.API_KEY}")
+    @Headers("Authorization: token " + BuildConfig.API_KEY)
     @GET("search/users")
         fun getGithubUsers(
             @Query("q") user: String
